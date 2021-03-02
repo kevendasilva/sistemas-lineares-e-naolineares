e5cc63fb53ce431406c3b282f32131ab60da226a
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2021-03-02 19:58:41 -0300

    Método de Newton-Raphson para sistemas não-linereas

--- sistemas-nao-lineares/class/main.rb
+++ sistemas-nao-lineares/class/main.rb
@@ -3,8 +3,41 @@ require_relative '../module/system'
 class Main
   include System
 
-  x1 = -1
-  y1 = 1.732
-  x2 = x1 + System.delta_x(x1, y1)
-  puts x2
+=begin
+  # Estimativas mais distantes da solução
+  x_i = 1 # Estimativas iniciais
+  y_i = 1
+=end
+
+  x_i = 1 # Estimativas iniciais
+  y_i = 2
+  err = 0.003 # Erro escolhido
+  n = 1 # Contador de iterações
+
+  puts "\n \n"
+  puts 'Solução de sistemas não-lineares'
+  puts "\n \n"
+  puts "A estimativa inicial é: (x, y) = (#{x_i}, #{y_i})"
+  puts "\n \n"
+
+  loop do
+    x_ip1 = x_i + System.delta_x(x_i, y_i) # x_ip1 é a próxima solução estimada
+    y_ip1 = y_i + System.delta_y(x_i, y_i) # y_ip1 é a próxima solução estimada
+    err_x = ((x_ip1 - x_i) / x_i).abs # Erro Relativo Estimado de x
+    err_y = ((y_ip1 - y_i) / y_i).abs # Erro Relativo Estimado de y
+    puts '*-*-*-*-*-*-*-*-*-*'
+    puts "Iteração de número #{n}"
+    puts '*-*-*-*-*-*-*-*-*-*'
+    puts 'O valor de y_ip1 é %0.06f e o erro é %0.06f' % [x_ip1, err_x]
+    puts '&'
+    puts 'O valor de y_ip1 é %0.06f e o erro é %0.06f' % [y_ip1, err_y]
+    puts '*-*-*-*-*-*-*-*-*-*'
+    puts "\n"
+    break if err_x < err && err_y < err # Caso os erros das estimativas seja menor que o erro estabelecido
+    # o laço termina.
+
+    n += 1 # Caso o laço não termine, novas soluções devem ser estimadas.
+    x_i = x_ip1
+    y_i = y_ip1
+  end
 end
--- sistemas-nao-lineares/module/system.rb
+++ sistemas-nao-lineares/module/system.rb
@@ -2,12 +2,12 @@
 module System
   # Função f1(x, y)
   def self.f1_x_y(coord_x, coord_y)
-    coord_x.pow(2) + coord_y.pow(2) - 4
+    coord_x * coord_x + coord_y * coord_y - 4
   end
 
   # Função f2(x, y)
   def self.f2_x_y(coord_x, coord_y)
-    coord_y - coord_x.pow(2) - 1
+    coord_y - coord_x * coord_x - 1
   end
 
   def self.partial_x_f1(coord_x, _coord_y)
@@ -35,7 +35,7 @@ module System
     d_x.to_f / j_f1_f2(coord_x, coord_y)
   end
 
-  def self.delta_y()
+  def self.delta_y(coord_x, coord_y)
     d_y = -f2_x_y(coord_x, coord_y) * partial_x_f1(coord_x, coord_y) + f1_x_y(coord_x, coord_y) * partial_x_f2(coord_x, coord_y)
     d_y.to_f / j_f1_f2(coord_x, coord_y)
   end
